56d40d7f811be842cc60d99416f1c79fc528150f
Katherine <46581738+KatherineZharkova@users.noreply.github.com>
2020-02-02 06:11:25 +0300

    Update NotesList.kt

--- app/src/main/java/ru/cocovella/mynotebook/model/entities/NotesList.kt
+++ app/src/main/java/ru/cocovella/mynotebook/model/entities/NotesList.kt
@@ -6,6 +6,6 @@ import java.util.*
 object NotesList {
     val notes: ArrayList<Note> = ArrayList()
 
-    override fun toString(): String = Arrays.deepToString(notes.toArray()
+    override fun toString(): String = Arrays.deepToString(notes.toArray())
 
 }
